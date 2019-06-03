8b4070a493d8c324aaf03ec24504434c520b9951
bj0rge <bastien.jorge@gmail.com>
2019-06-03 15:28:57 +0200

    fix: stop throwing errors if on wrong page

--- scripts/warn-on-github-if-hotfix-not-on-master.js
+++ scripts/warn-on-github-if-hotfix-not-on-master.js
@@ -3,17 +3,21 @@
 // @namespace    http://tampermonkey.net/
 // @updateURL    https://raw.githubusercontent.com/Spendesk/tampermonkey-scripts/master/scripts/warn-on-github-if-hotfix-not-on-master.js
 // @downloadURL  https://raw.githubusercontent.com/Spendesk/tampermonkey-scripts/master/scripts/warn-on-github-if-hotfix-not-on-master.js
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       Spendesk
 // @match        *github.com/*
 // @grant        none
+// @icon         https://www.spendesk.com/favicon-32x32.png
 // ==/UserScript==
 
 (function() {
     'use strict';
     const baseElement = document.querySelector('.commit-ref.css-truncate.user-select-contain.expandable.base-ref');
     const headElement = document.querySelector('.commit-ref.css-truncate.user-select-contain.expandable.head-ref');
+
+    const isOnPullRequest = baseElement && headElement;
+    if (!isOnPullRequest) return;
 
     const isHeadHotfix = headElement.innerText.includes('fix');
     const isBaseMaster = baseElement.innerText === 'master';
